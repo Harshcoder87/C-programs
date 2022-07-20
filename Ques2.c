4c1a448e5c9d76305c234383774e4332e546b23e
Harshcoder87 <109306929+Harshcoder87@users.noreply.github.com>
2022-07-20 18:20:35 +0530

    Add files via upload

---
+++ Ques2.c
@@ -0,0 +1,16 @@
+  // Online C compiler to run C program online
+  #include<stdio.h>
+   int main()  {
+     int a;
+      printf("Enter the number");
+       scanf("%d", &a);
+           if(a%5 ==0)
+            { 
+                printf("It's Divsible by five");
+            }
+             else{
+                printf("not Dvisble by 5");
+             }    printf("/n");
+              return 0;
+                          }
+  
